83fe50196cbab8a373fd8c35eb529997a16b7e30
ArturGainutdinov <gajnutdinov.a.l@ft-soft.ru>
2023-12-10 23:40:09 +0500

    jwt

--- client/src/nav/protected-route.tsx
+++ client/src/nav/protected-route.tsx
@@ -1,8 +1,25 @@
-import { PropsWithChildren } from "react";
-import { IndexRouteProps, Navigate, Outlet, Route, RouteProps } from "react-router-dom";
+import { PropsWithChildren, useEffect } from "react";
+import { IndexRouteProps, Navigate, Outlet, Route, RouteProps, useNavigate } from "react-router-dom";
+import { apiProvider } from "../provider/api-provider";
+import { Nav } from "@nav";
+import axios from "axios";
 
 export function PrivateRoute() {
     const auth = document.cookie;
+
+    const navigate = useNavigate();
+
+    function navigateToLogin() {
+        navigate(Nav.login());
+    }
+
+    useEffect(() => {
+        if (!auth) {
+            return navigateToLogin();
+        }
+        axios.defaults.headers.common.Authorization = auth;
+        apiProvider.auth.getProfile().catch(navigateToLogin);
+    }, []);
 
     return auth ? <Outlet /> : <Navigate to="/login" />;
 }
--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -14,7 +14,7 @@ import { JwtStrategy } from './jwt.strategy';
     PassportModule,
     JwtModule.register({
       secret: authConstants.jwt.secret,
-      signOptions: { expiresIn: '600s' },
+      signOptions: { expiresIn: '10s' },
     }),
   ],
   providers: [AuthService, LocalStrategy, JwtStrategy],
